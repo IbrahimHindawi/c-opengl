64ef2d7988ace99a915a96d6333d500e51cee344
IbrahimHindawi <ihindawi@gmail.com>
2023-02-02 08:04:59 +0400

    updated code and build and ignore

--- bbuild.bat
+++ bbuild.bat
@@ -8,11 +8,16 @@ set u32="C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\user32.l
 set g32="C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\gdi32.lib"
 set s32="C:\Program Files (x86)\Windows Kits\10\Lib\10.0.19041.0\um\x64\shell32.lib"
 
-rem program
-set root=C:\devel
-set inc=/I %root%\glfw\include\ /I %root%\glad\include\
-set mlibs=%root%\glfw\build\src\Debug\glfw3.lib
+rem paths
+set libroot=C:\devel
+set source=..\source
 
-cl /nologo /MDd /c /nologo %inc% ..\source\main.c %root%\glad\src\glad.c
-link /nologo %root%\gfx\build\main.obj %root%\gfx\build\glad.obj %mlibs% %k32% %u32% %g32% %s32%  
+rem program
+set inc=/I %libroot%\glfw\include\ /I %libroot%\glad\include\
+set slib=%libroot%\glfw\build\src\Debug\glfw3.lib
+set cfiles=%libroot%\glad\src\glad.c %source%\main.c 
+set ofiles=glad.obj main.obj
+
+cl /nologo /MDd /c /nologo %inc% %cfiles%
+link /nologo /out:main.exe %ofiles% %slib% %k32% %u32% %g32% %s32%  
 popd build
--- source/main.c
+++ source/main.c
@@ -10,6 +10,26 @@
 #define true 1
 #define false 0
 
+const char *vertex_shader_source = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+    "}\0";
+
+const char *fragment_shader_source = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.6f, 0.2f, 1.0f);\n"
+    "}\0";
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -39,11 +59,50 @@ int main() {
         glfwTerminate();
         return -1;
     }
+
+    uint32_t vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+
+    uint32_t fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+
+    uint32_t shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    uint32_t vbo;
+    glGenBuffers(1, &vbo);
+
+    uint32_t vao;
+    glGenVertexArrays(1, &vao);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while(!glfwWindowShouldClose(window)) {
         process_input(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+         
+
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwPollEvents();
         glfwSwapBuffers(window);
